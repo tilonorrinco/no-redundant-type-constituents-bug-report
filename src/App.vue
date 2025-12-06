b4b18fd8122d208790147d11ce851f97f85efd5b
Mikhail Denisov <esperanto22@gmail.com>
2025-12-07 01:20:52 +0500

    additional fix

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
 import SomeComponent from './SomeComponent.vue';
 
 type SomeComponentType = typeof SomeComponent;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 type ExtendedSomeComponentType = SomeComponentType & { hello: 'world' };
 // why @typescript-eslint/no-redundant-type-constituents error here?
 </script>
