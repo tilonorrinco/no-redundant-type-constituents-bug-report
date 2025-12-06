6d7be5dc0db6ae138e95c1bdbc484b22d603fc06
Mikhail Denisov <esperanto22@gmail.com>
2025-12-07 01:02:51 +0500

    minimal reprodusable error example

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app">
+    
+  </div>
+</template>
+
+<script setup lang="ts">
+import SomeComponent from './SomeComponent.vue';
+
+type SomeComponentType = typeof SomeComponent;
+type ExtendedSomeComponentType = SomeComponentType & { hello: 'world' };
+// why @typescript-eslint/no-redundant-type-constituents error here?
+</script>
+
---
+++ src/SomeComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="some-component">
+    {{ text }}
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  text: string;
+};
+
+defineProps<Props>()
+</script>
